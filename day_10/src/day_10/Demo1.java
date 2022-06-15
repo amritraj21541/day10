a791c200fa94f8e9a797c0e3a58d5697f0249458
amrit <amrit@MSI>
2022-06-15 13:46:06 +0530

    dat 10 done

---
+++ day_10/src/day_10/Demo1.java
@@ -0,0 +1,17 @@
+package day_10;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+			int a =10; 
+			
+			Integer i = Integer.valueOf(a);
+			
+			Integer k = a;
+			System.out.println(a + " "+ i + " "+ k);
+			
+
+
+	}
+
+}
